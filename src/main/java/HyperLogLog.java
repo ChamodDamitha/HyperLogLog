71b9c829b8b380e0f5c3473313de17e236daf602
chamod <chamod.14@cse.mrt.ac.lk>
2017-08-01 16:54:43 +0530

    more fair confidence interval returns

--- src/main/java/HyperLogLog.java
+++ src/main/java/HyperLogLog.java
@@ -1,20 +1,39 @@
-import java.lang.reflect.Array;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+/*
+* Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* WSO2 Inc. licenses this file to you under the Apache License,
+* Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
+import org.apache.log4j.Logger;
+
 
 /**
- * Created by chamod on 7/27/17.
+ * A probabilistic data structure to calculate cardinality of a set
+ * @param <E> is the type of objects in the set
  */
 public class HyperLogLog<E> {
+
+    private static final Logger LOG = Logger.getLogger(HyperLogLog.class);
 
     private double accuracy;
     private int noOfBuckets;
     private int lengthOfBucketId;
     private int[] countArray;
 
-    private final double ESTIMATION_FACTOR = 0.7;
-
-    private MessageDigest messageDigest;
+    private final double errorFactor = 1.04;
+    private double estimationFactor;
 
     /**
      * Create a new HyperLogLog by specifying the accuracy
@@ -24,59 +43,47 @@ public class HyperLogLog<E> {
     public HyperLogLog(double accuracy) {
         this.accuracy = accuracy;
 
-//      accuracy = 1.04 / sqrt(noOfBuckets) = > noOfBuckets = (1.04 / accuracy) ^ 2
-        noOfBuckets = (int)Math.ceil(Math.pow(1.04 / accuracy, 2));
-//        System.out.println("array size I : " + noOfBuckets); //TODO : added for testing
+//      accuracy = errorFactor / sqrt(noOfBuckets) = > noOfBuckets = (errorFactor / accuracy) ^ 2
+        noOfBuckets = (int) Math.ceil(Math.pow(errorFactor / accuracy, 2));
 
-        lengthOfBucketId = (int)Math.ceil(Math.log(noOfBuckets) / Math.log(2));
-//        System.out.println("bucket size : " + lengthOfBucketId); //TODO : added for testing
+        lengthOfBucketId = (int) Math.ceil(Math.log(noOfBuckets) / Math.log(2));
 
         noOfBuckets = (1 << lengthOfBucketId);
-        System.out.println("array size II : " + noOfBuckets); //TODO : added for testing
 
         countArray = new int[noOfBuckets];
 
-//        setting MD5 digest function to generate hashes
-        try {
-            this.messageDigest = MessageDigest.getInstance("MD5");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
+        estimationFactor = getEstimationFactor(lengthOfBucketId, noOfBuckets);
     }
 
     /**
      * Compute the accuracy using the count array size
-     * @return
+     * @return the accuracy value
      */
-    public double getAccuracy(){
-        return (1.04 / Math.sqrt(noOfBuckets));
+    public double getAccuracy() {
+        return (errorFactor / Math.sqrt(noOfBuckets));
     }
 
     /**
      * Calculate the cardinality(number of unique items in a set)
-     * by calculating the harmonic mean of the counts in the buckets
-     * Check for the upper and lower bounds to modify the estimation
-     * @return
+     * by calculating the harmonic mean of the counts in the buckets.
+     * Check for the upper and lower bounds to modify the estimation.
+     * @return the cardinality value
      */
-    public long getCardinality(){
-        printArray(); // TODO : added for testing
+    public long getCardinality() {
 
         double harmonicCountSum = 0;
         double harmonicCountMean;
         int noOfZeroBuckets = 0;
         int estimatedCardinality;
         long count;
+        long cardinality;
 
 //      calculate harmonic mean of the bucket values
-        for(int i = 0; i < noOfBuckets; i++){
+        for (int i = 0; i < noOfBuckets; i++) {
             count = countArray[i];
             harmonicCountSum += (1.0 / (1 << count));
 
-//            System.out.println("count : " + count); //TODO : added for testing
-//            System.out.println("harm plus : " + (1 << count)); //TODO : added for testing
-
-            if(count == 0){
+            if (count == 0) {
                 noOfZeroBuckets++;
             }
         }
@@ -84,43 +91,43 @@ public class HyperLogLog<E> {
         harmonicCountMean = noOfBuckets / harmonicCountSum;
 
 //      calculate the estimated cardinality
-        estimatedCardinality = (int)Math.ceil(noOfBuckets * ESTIMATION_FACTOR * harmonicCountMean);
-        System.out.println("Estimated Cardinality : " + estimatedCardinality); // TODO : added for testing
-        System.out.println("((1 << 32) / 30.0) : " + (Math.pow(2,32) / 30.0)); // TODO : added for testing
+        estimatedCardinality = (int) Math.ceil(noOfBuckets * estimationFactor * harmonicCountMean);
 
+        final double pow2to32 = Math.pow(2, 32);
 
 //      if the estimate E is less than 2.5 * 32 and there are buckets with max-leading-zero count of zero,
 //      then instead return −32⋅log(V/32), where V is the number of buckets with max-leading-zero count = 0.
-        if((estimatedCardinality < 2.5 * noOfBuckets) && noOfZeroBuckets > 0){       //threshold of 2.5x comes from the recommended load factor for Linear Counting
-            return (int)Math.ceil(-noOfBuckets * Math.log((double) noOfZeroBuckets / noOfBuckets));
+//      threshold of 2.5x comes from the recommended load factor for Linear Counting
+        if ((estimatedCardinality < 2.5 * noOfBuckets) && noOfZeroBuckets > 0) {
+//            cardinality =  (long)(noOfBuckets * Math.log((double) noOfBuckets / noOfZeroBuckets));
+            cardinality = (int) (-noOfBuckets * Math.log((double) noOfZeroBuckets / noOfBuckets));
+        } else if (estimatedCardinality > (pow2to32 / 30.0)) {
+            //       if E > 2 ^ (32) / 30 : return −2 ^ (32) * log(1 − E / 2 ^ (32))
+            cardinality = (int) Math.ceil(-(pow2to32 * Math.log(1 - (estimatedCardinality / (pow2to32)))));
+        } else {
+            cardinality = estimatedCardinality;
         }
-//       if E > 2 ^ (32) / 30 : return −2 ^ (32) * log(1 − E / 2 ^ (32))
-        else if(estimatedCardinality > (Math.pow(2,32) / 30.0)){
-            return (int)Math.ceil(-(Math.pow(2,32) * Math.log(1 - (estimatedCardinality / (Math.pow(2,32))))));
-        }
-        else{
-            return estimatedCardinality;
-        }
+
+//        String confidenceInterval = String.format("confidence interval of cardinality : [%s, %s]",
+//        cardinality - cardinality * getAccuracy(), cardinality + cardinality * getAccuracy());
+
+        return cardinality;
     }
 
     /**
      * Adds a new item to the array by hashing and setting the count of relevant bucckets
      * @param item
      */
-    public void addItem(E item){
+    public void addItem(E item) {
         int hash = getHashValue(item);
-//        System.out.println("hash : " + Integer.toBinaryString(hash)); //TODO : added for testing
 
 //      Shift all the bits to right till only the bucket ID is left
         int bucketId = hash >>> (Integer.SIZE - lengthOfBucketId);
-//        System.out.println("bucketID : " + Integer.toBinaryString(bucketId)); //TODO : added for testing
 
 //      Shift all the bits to left till the bucket id is removed
         int remainingValue = hash << lengthOfBucketId | (1 << (lengthOfBucketId - 1));
-//        System.out.println("Remaining value : " + Integer.toBinaryString(remainingValue)); //TODO : added for testing
 
         int noOfLeadingZeros = Integer.numberOfLeadingZeros(remainingValue) + 1;
-//        System.out.println("no of leading zeros : " + noOfLeadingZeros); //TODO : added for testing
 
         updateBucket(bucketId, noOfLeadingZeros);
     }
@@ -129,23 +136,25 @@ public class HyperLogLog<E> {
      * Update the zero count value in the relevant bucket if the given value is larger than the existing value
      * @param index is the bucket ID of the relevant bucket
      * @param leadingZeroCount is the new zero count
+     * @return {@code true} if the bucket is updated, {@code false} if the bucket is not updated
      */
-    private void updateBucket(int index, int leadingZeroCount){
+    private boolean updateBucket(int index, int leadingZeroCount) {
         long currentZeroCount = countArray[index];
-        if(currentZeroCount < leadingZeroCount){
+        if (currentZeroCount < leadingZeroCount) {
             countArray[index] = leadingZeroCount;
+            return true;
         }
+        return false;
     }
 
 
     /**
      * Compute an integer hash value for a given value
      * @param value to be hashed
-     * @return
+     * @return integer hash value
      */
-    public int getHashValue(E value){
-        byte[] bytes = messageDigest.digest(getBytes(value));
-        return MurmurHash.hash32(bytes, 10);
+    public int getHashValue(E value) {
+        return MurmurHash.hash(value);
     }
 
     /**
@@ -153,18 +162,29 @@ public class HyperLogLog<E> {
      * @param data
      * @return a byte array
      */
-    private byte[] getBytes(E data){
+    private byte[] getBytes(E data) {
         return data.toString().getBytes();
     }
 
     /**
      * Print the contents of the count array
      */
-    private void printArray(){
-        for(long x : countArray){
-            System.out.print(x + ", ");
+//    private void printArray() {
+//        for (long x : countArray) {
+//        }
+//    }
+
+    private double getEstimationFactor(int lengthOfBucketId, int noOfBuckets) {
+        switch (lengthOfBucketId) {
+            case 4:
+                return 0.673;
+            case 5:
+                return 0.697;
+            case 6:
+                return 0.709;
+            default:
+                return (0.7213 / (1 + 1.079 / noOfBuckets));
         }
-        System.out.println();
     }
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,56 @@
+//import com.clearspring.analytics.stream.cardinality.*;
+//import com.clearspring.analytics.stream.cardinality.HyperLogLog;
+
 /**
  * Created by chamod on 7/27/17.
  */
 public class Main {
     public static void main(String[] args) {
 
-        HyperLogLog<Integer> integerHyperLogLog = new HyperLogLog<Integer>(0.05);
+        final int NO_OF_UNIQUE_NUMBERS = 200000;
 
-        for(int i = 0; i < 20; i++){
+        HyperLogLog<Integer> integerHyperLogLog = new HyperLogLog<Integer>(0.09);
+
+        for(int i = 0; i < NO_OF_UNIQUE_NUMBERS; i++){
             integerHyperLogLog.addItem(i);
         }
 
+//        for(int i = 0; i < NO_OF_UNIQUE_NUMBERS; i++){
+//            integerHyperLogLog.addItem(i);
+//        }
+//        for(int i = 0; i < NO_OF_UNIQUE_NUMBERS; i++){
+//            integerHyperLogLog.addItem(i);
+//        }
+//        for(int i = 0; i < NO_OF_UNIQUE_NUMBERS; i++){
+//            integerHyperLogLog.addItem(i);
+//        }
+
         System.out.println("cardinality : " + integerHyperLogLog.getCardinality());
+        System.out.println("accuracy : " + integerHyperLogLog.getAccuracy());
+
+        long cardinality  = integerHyperLogLog.getCardinality();
+        double accuracy = integerHyperLogLog.getAccuracy();
+        System.out.println(String.format("[%s, %s]", cardinality - cardinality * accuracy,
+                cardinality + cardinality * accuracy));
+
+
+
+        //............................library................................
+//
+//        int log2m = 30;
+//
+//        double accuracy = 1.04 / Math.sqrt(1 << log2m);
+//
+//
+//        com.clearspring.analytics.stream.cardinality.HyperLogLog hyperLogLog = new HyperLogLog(log2m);
+//
+//        for(int i = 0; i < NO_OF_UNIQUE_NUMBERS; i++){
+//            hyperLogLog.offer(i);
+//        }
+//
+//        long cardinality  = hyperLogLog.cardinality();
+//        System.out.println("cardinality : " + hyperLogLog.cardinality());
+////        System.out.println("accuracy : " + integerHyperLogLog.getAccuracy());
+//        System.out.println(String.format("[%s, %s]", cardinality - cardinality * accuracy, cardinality + cardinality * accuracy));
     }
 }
